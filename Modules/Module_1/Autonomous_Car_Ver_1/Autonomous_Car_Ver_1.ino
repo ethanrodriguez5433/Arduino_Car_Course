a079bd69e4d7981fef9a235aa01f1b1a92c6ca2c
DonovanAcosta <Donovan25@outlook.com>
2024-10-28 19:04:21 -0500

    New code for new motors

---
+++ Modules/Module_1/Autonomous_Car_Ver_1/Autonomous_Car_Ver_1.ino
@@ -0,0 +1,72 @@
+#define PIN1 6
+#define PIN2 7
+#define PIN3 10
+#define PIN4 11
+void setup() {
+  pinMode(PIN1, OUTPUT);  //PIN 1 FOR MOTOR 1
+  pinMode(PIN2, OUTPUT);  //PIN 2 FOR MOTOR 1
+  pinMode(PIN3, OUTPUT);  //PIN 3 FOR MOTOR 2
+  pinMode(PIN4, OUTPUT);  //PIN 4 FOR MOTOR 2
+}
+
+void loop() {
+  int randMov;
+  randMov = random(0, 3);
+  
+  if(randMov == 0)
+  {
+    moveForward();
+  }
+  else if(randMov == 1)
+  {
+    moveBackward();
+  }
+  else if(randMov == 2)
+  {
+    moveLeft();
+  } 
+  else
+  {
+    moveRight();
+  }
+}
+
+void moveForward()
+{
+  //set pins 1 & 4 to low and 2 & 3 to high
+  digitalWrite(PIN1, LOW);
+  digitalWrite(PIN2, HIGH);
+  digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, LOW);
+  delay(1000);
+}
+
+void moveBackward()
+{
+  //set pins 2 & 3 to low and 1 & 4 to high
+  digitalWrite(PIN1, HIGH);
+  digitalWrite(PIN2, LOW);
+  digitalWrite(PIN3, LOW);
+  digitalWrite(PIN4, HIGH);
+  delay(1000);
+}
+
+void moveLeft()
+{
+  //set pins 1 & 3 to high and 2 & 4 to low 
+  digitalWrite(PIN1, HIGH);
+  digitalWrite(PIN2, LOW);
+  digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, LOW);
+  delay(1000);
+}
+
+void moveRight()
+{
+  //set pins 2 & 4 to high and 1 & 3 to low
+  digitalWrite(PIN1, LOW);
+  digitalWrite(PIN2, HIGH);
+  digitalWrite(PIN3, LOW);
+  digitalWrite(PIN4, HIGH);
+  delay(1000);
+}
---
+++ Modules/Module_2/Autonomous_Car_Ver_2/Autonomous_Car_Ver_2.ino
@@ -0,0 +1,75 @@
+
+//Define motor/driver inputs
+#define PIN1 6
+#define PIN2 7
+#define PIN3 10
+#define PIN4 11
+
+#define trigPin 9
+#define echoPin 8
+
+//Set up runs only once, immediately when arduino is powered
+void setup() 
+{
+  Serial.begin(9600);          // Initialize serial communication at 9600 bps
+  pinMode(trigPin, OUTPUT);     // Set trigPin as an output
+  pinMode(echoPin, INPUT);      // Set echoPin as an input
+  
+  pinMode(PIN1, OUTPUT);  //PIN 1 FOR MOTOR 1
+  pinMode(PIN2, OUTPUT);  //PIN 2 FOR MOTOR 1
+  pinMode(PIN3, OUTPUT);  //PIN 3 FOR MOTOR 2
+  pinMode(PIN4, OUTPUT);  //PIN 4 FOR MOTOR 2
+}
+
+void loop() {
+  long duration, inches, cm;
+
+  // Trigger the ultrasonic burst
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Measure the time it takes for the echo to return
+  duration = pulseIn(echoPin, HIGH);
+
+  // Convert duration to centimeters
+  cm = microsecondsToCentimeters(duration);
+
+  // Print the distance to the Serial Monitor
+  Serial.print(cm);
+  Serial.print(" cm");
+  Serial.println();
+
+  if(cm <= 10)
+    moveLeft();
+  else
+    moveForward();
+
+}
+
+// Function to convert microseconds to centimeters
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;   // Speed of sound in air (cm per microsecond)
+}
+
+void moveForward()
+{
+  //set pins 1 & 4 to low and 2 & 3 to high
+  digitalWrite(PIN1, LOW);
+  digitalWrite(PIN2, HIGH);
+  digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, LOW);
+  delay(1000);
+}
+
+void moveLeft()
+{
+  //set pins 1 & 3 to high and 2 & 4 to low 
+  digitalWrite(PIN1, HIGH);
+  digitalWrite(PIN2, LOW);
+  digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, LOW);
+  delay(1000);
+}
