fbd553fbe4b9062fe425b6ae77d96e5f82cc1895
DonovanAcosta <Donovan25@outlook.com>
2024-11-06 13:21:18 -0600

    Add comments to module 1

--- Modules/Module_1/Autonomous_Car_Ver_1/Autonomous_Car_Ver_1.ino
+++ Modules/Module_1/Autonomous_Car_Ver_1/Autonomous_Car_Ver_1.ino
@@ -1,7 +1,20 @@
+//**********************************************************/
+// Edit Code Below
+//Write the numbers on the arduino Associated with the 
+//four pins for the motors next to each define statement
+//
+//Ex: #define PIN1 13
+//**********************************************************/
+
 #define PIN1 10
 #define PIN2 11
 #define PIN3 6
 #define PIN4 7
+/**********************************************************/
+//
+/**********************************************************/
+
+
 void setup() {
   pinMode(PIN1, OUTPUT);  //PIN 1 FOR MOTOR 1
   pinMode(PIN2, OUTPUT);  //PIN 2 FOR MOTOR 1
@@ -9,7 +22,8 @@ void setup() {
   pinMode(PIN4, OUTPUT);  //PIN 4 FOR MOTOR 2
 }
 
-void loop() {
+void loop() 
+{
   int randMov;
   randMov = random(0, 3);
   
@@ -25,29 +39,39 @@ void loop() {
   {
     moveLeft();
   } 
-  else
+  else if(randMov == 3)
   {
     moveRight();
   }
 }
 
+
+//***************************************/
+//Edit Code below
+//Change the assignment of the pins below (HIGH/LOW)
+//so that both motors move in the proper direction
+//
+//Use moveForward() as an example
+//**************************************/
+
+
 void moveForward()
 {
   //set pins 1 & 4 to low and 2 & 3 to high
-  digitalWrite(PIN1, LOW);
-  digitalWrite(PIN2, HIGH);
-  digitalWrite(PIN3, LOW);
-  digitalWrite(PIN4, HIGH);
+  digitalWrite(PIN1, HIGH);
+  digitalWrite(PIN2, LOW);
+  digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, LOW);
   delay(1000);
 }
 
 void moveBackward()
 {
   //set pins 2 & 3 to low and 1 & 4 to high
-  digitalWrite(PIN1, HIGH);
-  digitalWrite(PIN2, LOW);
-  digitalWrite(PIN3, HIGH);
-  digitalWrite(PIN4, LOW);
+  digitalWrite(PIN1, LOW);
+  digitalWrite(PIN2, HIGH);
+  digitalWrite(PIN3, LOW);
+  digitalWrite(PIN4, HIGH);
   delay(1000);
 }
 
@@ -58,7 +82,7 @@ void moveLeft()
   digitalWrite(PIN2, LOW);
   digitalWrite(PIN3, LOW);
   digitalWrite(PIN4, HIGH);
-  delay(1000);
+  delay(300);
 }
 
 void moveRight()
@@ -67,6 +91,27 @@ void moveRight()
   digitalWrite(PIN1, LOW);
   digitalWrite(PIN2, HIGH);
   digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, LOW);
+  delay(300);
+}
+
+
+//**********************************************
+//For more practice, change the value in delay()
+//This will affect how long each motion is performed
+//ex: delay(600)
+//
+//Try changing the delay so that in one turn
+//the car rotates 90 degrees
+//Hint: You may need to utilize stopCar()
+//Hint: delay() takes a number of miliseconds (600 = 600 miliseconds)
+//***********************************************
+
+void stopCar()
+{
+  digitalWrite(PIN1, LOW);
+  digitalWrite(PIN2, LOW);
+  digitalWrite(PIN3, LOW);
   digitalWrite(PIN4, LOW);
   delay(1000);
 }
